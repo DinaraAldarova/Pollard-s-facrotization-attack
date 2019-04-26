05d0c7b41605962875507d0eedb070bdf728d214
Динара Алдарова <dinara_aldarova@mail.ru>
2019-04-26 15:04:53 +0500

    Добавьте файлы проекта.

---
+++ Pollard's facrotization attack/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pollard_s_facrotization_attack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
